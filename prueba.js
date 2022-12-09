b1cf714054dab887987f8eea004067d555b24e8c
AngelMancha <100451302@alumnos.uc3m.es>
2022-12-09 14:58:07 +0100

    Cambios en el footer y botón de perfil desplegable añadido

--- prueba.js
+++ prueba.js
@@ -24,6 +24,7 @@ function rep_despe() {
     music.src = "./archivos/canciones/rosalia/despecha.mp3"
     document.getElementById("artista").innerHTML = "Rosalia"
     document.getElementById("cancion").innerHTML = "Despechá"
+    document.getElementById("footer_foto").src = ("./archivos/imagenes/rosalia/despecha.jpg")
     reproducir()
 }
 
@@ -33,6 +34,7 @@ function rep_saoko() {
     music.src=("./archivos/canciones/rosalia/saoko.mp3");
     document.getElementById("artista").innerHTML = "Rosalia"
     document.getElementById("cancion").innerHTML = "Saoko"
+    document.getElementById("footer_foto").src = ("./archivos/imagenes/rosalia/saoko.png")
     reproducir()
 }
 function rep_biz() {
@@ -40,6 +42,7 @@ function rep_biz() {
     music.src=("./archivos/canciones/rosalia/bizcochito.mp3");
     document.getElementById("artista").innerHTML = "Rosalia"
     document.getElementById("cancion").innerHTML = "Bizcochito"
+    document.getElementById("footer_foto").src = ("./archivos/imagenes/rosalia/bizcochito.jpg")
     reproducir()
 }
 function rep_dinero() {
@@ -47,6 +50,7 @@ function rep_dinero() {
     music.src=("./archivos/canciones/rosalia/dinero.mp3");
     document.getElementById("artista").innerHTML = "Rosalia"
     document.getElementById("cancion").innerHTML = "Dinero"
+    document.getElementById("footer_foto").src = ("./archivos/imagenes/rosalia/dinero.jpg")
     reproducir()
 }
 function rep_altura() {
@@ -54,6 +58,7 @@ function rep_altura() {
     music.src=("./archivos/canciones/rosalia/altura.mp3");
     document.getElementById("artista").innerHTML = "Rosalia"
     document.getElementById("cancion").innerHTML = "Con altura"
+    document.getElementById("footer_foto").src = ("./archivos/imagenes/rosalia/altura.jpg")
     reproducir()
 }
 
@@ -62,6 +67,7 @@ function rep_kimi() {
     music.src=("./archivos/canciones/anime/kiminouso.mp3");
     document.getElementById("artista").innerHTML = ""
     document.getElementById("cancion").innerHTML = "Kimi Na Wa"
+    document.getElementById("footer_foto").src = ("./archivos/imagenes/Anime/kiminawa.jpg")
     reproducir()
 }
 
@@ -70,6 +76,7 @@ function rep_cruel() {
     music.src=("./archivos/canciones/anime/cruelangel.mp3");
     document.getElementById("artista").innerHTML = ""
     document.getElementById("cancion").innerHTML = "A Cruel Angel´s Thesis"
+    document.getElementById("footer_foto").src = ("./archivos/imagenes/Anime/cruelangel.jpg")
     reproducir()
 }
 
@@ -78,6 +85,7 @@ function rep_gurenge() {
     music.src=("./archivos/canciones/anime/gurenge.mp3");
     document.getElementById("artista").innerHTML = ""
     document.getElementById("cancion").innerHTML = "Gurenge"
+    document.getElementById("footer_foto").src = ("./archivos/imagenes/Anime/gurenge.jpg")
     reproducir()
 }
 
@@ -86,6 +94,7 @@ function rep_shinzo() {
     music.src=("./archivos/canciones/anime/shinzowosasageyo.mp3");
     document.getElementById("artista").innerHTML = ""
     document.getElementById("cancion").innerHTML = "Sinzo Wo Ssageyo"
+    document.getElementById("footer_foto").src = ("./archivos/imagenes/Anime/shinzowosasageyo.jpg")
     reproducir()
 }
 
@@ -94,6 +103,7 @@ function rep_shiga() {
     music.src=("./archivos/canciones/anime/yourname.mp3");
     document.getElementById("artista").innerHTML = ""
     document.getElementById("cancion").innerHTML = "Shigatsu Wa Kimi No Uso"
+    document.getElementById("footer_foto").src = ("./archivos/imagenes/Anime/yourlieinapril.jpg")
     reproducir()
 }
 
@@ -102,6 +112,7 @@ function rep_baby() {
     music.src=("./archivos/canciones/TikTok/baby_otaku.mp3");
     document.getElementById("artista").innerHTML = ""
     document.getElementById("cancion").innerHTML = "Baby Otaku"
+    document.getElementById("footer_foto").src = ("./archivos/imagenes/Tiktok/otaku.jpg")
     reproducir()
 }
 
@@ -110,6 +121,7 @@ function rep_freaky() {
     music.src=("./archivos/canciones/TikTok/freaky_girl.mp3");
     document.getElementById("artista").innerHTML = "Nicki Minaj"
     document.getElementById("cancion").innerHTML = "Freaky Girl"
+    document.getElementById("footer_foto").src = ("./archivos/imagenes/Tiktok/freaky.jpg")
     reproducir()
 }
 
@@ -118,6 +130,7 @@ function rep_unholy() {
     music.src=("./archivos/canciones/TikTok/UNHOLY.mp3");
     document.getElementById("artista").innerHTML = "Sam Smith - Kim Petras"
     document.getElementById("cancion").innerHTML = "Unholy"
+    document.getElementById("footer_foto").src = ("./archivos/imagenes/Tiktok/unholy.jfif")
     reproducir()
 }
 
@@ -126,6 +139,7 @@ function rep_kulosa() {
     music.src=("./archivos/canciones/TikTok/KULOSA.mp3");
     document.getElementById("artista").innerHTML = ""
     document.getElementById("cancion").innerHTML = "Kulosa"
+    document.getElementById("footer_foto").src = ("./archivos/imagenes/Tiktok/Kulosa.jfif")
     reproducir()
 }
 
@@ -134,5 +148,24 @@ function rep_cbat() {
     music.src=("./archivos/canciones/TikTok/cbat.mp3");
     document.getElementById("artista").innerHTML = ""
     document.getElementById("cancion").innerHTML = "Cbat"
+    document.getElementById("footer_foto").src = ("./archivos/imagenes/Tiktok/cbat.jfif")
     reproducir()
+}
+
+function myFunction() {
+    document.getElementById("myDropdown").classList.toggle("show");
+}
+
+// Close the dropdown if the user clicks outside of it
+window.onclick = function(event) {
+    if (!event.target.matches('.dropbtn')) {
+        var dropdowns = document.getElementsByClassName("dropdown-content");
+        var i;
+        for (i = 0; i < dropdowns.length; i++) {
+            var openDropdown = dropdowns[i];
+            if (openDropdown.classList.contains('show')) {
+                openDropdown.classList.remove('show');
+            }
+        }
+    }
 }
