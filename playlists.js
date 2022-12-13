e670d600deaf398316d1091778a819c873383588
RuthNavarroCarrasco <SU-CORREO-ELECTRONICO>
2022-12-13 13:15:41 +0100

    Valores de playlist guardados con éxito en local storage

--- playlists.js
+++ playlists.js
@@ -1,1 +1,10 @@
 /* Documento js para implementar la funcionalidad de las playlists */
+
+function setPlaylist(){
+    let playlist_name = document.getElementById("nombre_playlist").value;
+    let song = document.getElementById("buscar_cancion").value;
+
+    localStorage.setItem("Playlist", playlist_name);
+    localStorage.setItem("Song", song);
+
+}
